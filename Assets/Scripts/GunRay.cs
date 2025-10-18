17003ca1c989893239bc1b4d6adda4eec7cb8f26
AkaishiTakumi <115193708+AkaishiTakumi@users.noreply.github.com>
2025-10-18 14:53:56 +0900

    update: 銃から弾が発射するC#ファイルを作成
    銃から発射された弾がEnemyにヒットしたらExplosionを出現される

---
+++ Assets/Scripts/GunRay.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniGLTF;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class GunRay: MonoBehaviour
+{
+    private Ray ray;//‡BRayŒ^‚Ì•Ï”
+    public GameObject Explosion;//‡B“G‚ğ“|‚µ‚½‚ÉoŒ»‚³‚¹‚é”š”­
+    private int distance = 10000;//‡BRay‚ğ”ò‚Î‚·‹——£
+    private RaycastHit hit;//‡BRay‚ª‰½‚©‚É“–‚½‚Á‚½‚Ìî•ñ
+    public GameObject Muzzle;//‡BRay‚ğ”­Ë‚·‚éêŠ
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))//‡Bƒ}ƒEƒX‚ª¶ƒNƒŠƒbƒN‚³‚ê‚½‚ç 
+        {
+            Debug.Log("ƒNƒŠƒbƒN‚³‚ê‚½‚æ");
+            Ray ray = new Ray(Muzzle.transform.position, Muzzle.transform.forward);//‡BRay‚ğMuzzle‚ÌêŠ‚©‚ç‘O•û‚É”ò‚Î‚·
+            //Ray ray = new Ray(transform.position, transform.forward);//‡BRay‚ğ‚±‚ÌêŠ‚©‚ç‘O•û‚É”ò‚Î‚·
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);//‡BRay‚ğÔF‚Å•\¦‚³‚¹‚é
+
+            if (Physics.Raycast(ray, out hit, distance))
+            {//‡BRay‚ªdistance‚Ì”ÍˆÍ“à‚Å‰½‚©‚É“–‚½‚Á‚½‚É
+                Debug.Log("Ray‚ª“–‚½‚Á‚½‚æ");
+                if (hit.collider.tag == "Enemy")//‡B‚à‚µ“–‚½‚Á‚½•¨‚Ìƒ^ƒO‚ªEnemy‚¾‚Á‚½‚ç 
+                {
+                    Debug.Log("“G‚É“–‚½‚Á‚½‚æ");
+                    Destroy(hit.collider.gameObject);//‡B“–‚½‚Á‚½•¨‚ğÁ‹‚µ‚ÄA
+                    Instantiate(Explosion.gameObject, hit.collider.gameObject.transform.position, gameObject.transform.rotation);//‡BRay‚ª“–‚½‚Á‚½êŠ‚É”š”­‚ğ¶¬‚·‚é
+                }
+            }
+        }
+    }
+}
